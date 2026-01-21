6418e306ec82f15057b9d97d02d5240db5a3665a
YafeeRayyanBasunia <noushin9999@gmail.com>
2026-01-21 19:47:24 +0600

    Add files via upload

---
+++ Exam.py
@@ -0,0 +1,19 @@
+c = input("Are you have any medical cause yes/No: ")
+if c == "yes":
+    p = int(input("Enter your percentage: "))
+    if p>55:
+        print("You are allowed")
+    else:
+        print("Sorry, but you are not allowed") 
+if c == "No":
+    pp = int(input("Enter your percentage: "))
+    if pp>80:
+        print("You are allowed")
+    else:
+        print("You are not allowed")    
+
+
+
+
+
+
